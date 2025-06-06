7611a37fc16c94a70a3b8b2e282f1e7046df4dff
Yehor Dudnik <yehor.dudnik@gmail.com>
2025-06-06 22:19:07 +0100

    Fix final final final

--- src/main.py
+++ src/main.py
@@ -6,27 +6,22 @@ from telegram.ext import Application, MessageHandler, CommandHandler, filters
 from src.handlers import start, record, summary, reply_to_mentions, periodic_message, daily_summary
 
 
+async def setup_jobs(application: Application) -> None:
+    application.job_queue.run_repeating(periodic_message, interval=10800, first=5)
+
+
 def main():
     load_dotenv()
     token = os.getenv('BOT_TOKEN')
     if not token:
         raise ValueError("Environment variable BOT_TOKEN not found")
 
-
-    application = Application.builder().token(token).build()
+    application = Application.builder().token(token).post_init(setup_jobs).build()
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("record", record))
     application.add_handler(CommandHandler("summary", summary))
-
     application.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), reply_to_mentions))
-    application.job_queue.run_repeating(periodic_message, interval=10800, first=5)
-
-    #    application.job_queue.run_daily(
-#        daily_summary,
-#       time=time(23, 0),
-#        name="daily_reminder"
-#   )
 
     print("Bot started!")
     application.run_polling()
